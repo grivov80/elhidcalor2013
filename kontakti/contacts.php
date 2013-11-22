d9e8b7c69f249236f02e8a527bddd5dfdfe4b149
Georgi Grivov <georgi.grivov@gmail.com>
2013-11-22 11:57:39 +0200

    t555

---
+++ kontakti/contacts.php
@@ -0,0 +1,66 @@
+<?php 
+ include '../config.php';
+?>
+<!Doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?php echo COMPANYNAME;?> - Контакти</title>
+    <meta id="Keywords" name="keywords" content="Елхидкалор, магазин в Асеновград за Електро, В и К, Отопление.
+     Контакти">
+    <meta id="Description" name="description" content="Контакти">
+    <link rel="stylesheet" type="text/css" href="../mcss/styles.css">
+    
+<!-- google analytics -->
+<?php include '../ehcgoogleanalytics.html';?>
+</head>
+<body>
+<header>
+  <h1><a href="../index.php"><?php  echo COMPANYNAME;?></a></h1>
+   <?php include '../horizontalnoMenu.php';?>
+</header>
+<table id="tblMain">
+<tr valign="top">
+<td width="100px">
+<?php include '../verticalnoMenu.php';?>
+</td>
+
+<td>
+ <?php //include '../../whereamiVik.php';?>
+ <?php //echo"</div>";?>
+<br><br>
+
+<!-- body Col -->
+<h1>ЕЛХИДКАЛОР ЕООД</h1>
+<address>
+<p>Офис</p>
+<p>гр. Пловдив</p>
+<p>Адрес: ГС. РАКОВСКИ 27</p>
+<p>Лице за контакт: Андрей Кошеров</p>
+<p>моб.: +359 (876) 460 797  </p>
+<hr>
+<p>Магазин и склад</p>
+<p>гр. Асеновград</p>
+<p>Адрес: Шести Януари 29</p>
+<p>Лице за контакт: Андрей Кошеров</p>
+<p>моб.: +359 (876) 460 797  </p>
+<p>тел.: 0331 91736  </p>
+<p>email: <a href="mailto:info@elhidcalor.com">info@elhidcalor.com</a>
+</p>
+<p>skype: elhidcalor</p>
+<p><a href="map.php">Виж картата</a></p>
+</address>
+</td>
+</tr>
+</table>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
---
+++ kontakti/map.php
@@ -0,0 +1,48 @@
+<?php 
+ include '../config.php';
+?>
+<!Doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?php echo COMPANYNAME;?> - Карта</title>
+    <meta id="Keywords" name="keywords" content="Елхидкалор, магазин в Асеновград за Електро, В и К, Отопление.
+     Карта">
+    <meta id="Description" name="description" content="Карта">
+    <link rel="stylesheet" type="text/css" href="../mcss/styles.css">
+    
+<!-- google analytics -->
+<?php include '../ehcgoogleanalytics.html';?>
+</head>
+<body>
+<header>
+  <h1><a href="../index.php"><?php  echo COMPANYNAME;?></a></h1>
+   <?php include '../horizontalnoMenu.php';?>
+</header>
+<table id="tblMain">
+<tr valign="top">
+<td width="100px">
+<?php include '../verticalnoMenu.php';?>
+</td>
+
+<td>
+ <?php //include '../../whereamiVik.php';?>
+ <?php //echo"</div>";?>
+<br><br>
+
+<iframe width="625" height="550" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.bg/maps?f=q&amp;source=s_q&amp;hl=bg&amp;geocode=&amp;q=%D1%83%D0%BB.+6-%D1%82%D0%B8+%D1%8F%D0%BD%D1%83%D0%B0%D1%80%D0%B8,+%D0%90%D1%81%D0%B5%D0%BD%D0%BE%D0%B2%D0%B3%D1%80%D0%B0%D0%B4&amp;aq=&amp;sll=42.004667,24.870609&amp;sspn=0.013458,0.042272&amp;g=%D1%83%D0%BB.+6-%D1%82%D0%B8+%D1%8F%D0%BD%D1%83%D0%B0%D1%80%D0%B8,+%D0%90%D1%81%D0%B5%D0%BD%D0%BE%D0%B2%D0%B3%D1%80%D0%B0%D0%B4,+%D0%9F%D0%BB%D0%BE%D0%B2%D0%B4%D0%B8%D0%B2&amp;ie=UTF8&amp;hq=&amp;hnear=%D1%83%D0%BB.+6-%D1%82%D0%B8+%D1%8F%D0%BD%D1%83%D0%B0%D1%80%D0%B8,+%D0%90%D1%81%D0%B5%D0%BD%D0%BE%D0%B2%D0%B3%D1%80%D0%B0%D0%B4,+%D0%9F%D0%BB%D0%BE%D0%B2%D0%B4%D0%B8%D0%B2&amp;ll=42.004667,24.870609&amp;spn=0.006729,0.021136&amp;z=14&amp;output=embed"></iframe><br /><small><a href="http://maps.google.bg/maps?f=q&amp;source=embed&amp;hl=bg&amp;geocode=&amp;q=%D1%83%D0%BB.+6-%D1%82%D0%B8+%D1%8F%D0%BD%D1%83%D0%B0%D1%80%D0%B8,+%D0%90%D1%81%D0%B5%D0%BD%D0%BE%D0%B2%D0%B3%D1%80%D0%B0%D0%B4&amp;aq=&amp;sll=42.004667,24.870609&amp;sspn=0.013458,0.042272&amp;g=%D1%83%D0%BB.+6-%D1%82%D0%B8+%D1%8F%D0%BD%D1%83%D0%B0%D1%80%D0%B8,+%D0%90%D1%81%D0%B5%D0%BD%D0%BE%D0%B2%D0%B3%D1%80%D0%B0%D0%B4,+%D0%9F%D0%BB%D0%BE%D0%B2%D0%B4%D0%B8%D0%B2&amp;ie=UTF8&amp;hq=&amp;hnear=%D1%83%D0%BB.+6-%D1%82%D0%B8+%D1%8F%D0%BD%D1%83%D0%B0%D1%80%D0%B8,+%D0%90%D1%81%D0%B5%D0%BD%D0%BE%D0%B2%D0%B3%D1%80%D0%B0%D0%B4,+%D0%9F%D0%BB%D0%BE%D0%B2%D0%B4%D0%B8%D0%B2&amp;ll=42.004667,24.870609&amp;spn=0.006729,0.021136&amp;z=14" style="color:#0000FF;text-align:left">Вижте по-голяма карта</a></small>
+</td>
+</tr>
+</table>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
